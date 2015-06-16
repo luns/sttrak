7b870c59d81b733574bab2ab8890288c5bc71125
luns <luns@list.ru>
2015-06-16 09:06:26 +0300

    Создание репозитория
    Содержит "пустой" сайт RoR с подключенным bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sttrak::Application.config.secret_key_base = 'ef5e3e704bed07265814ceea862cb14b0f332db6920cc3f8f354d4ac4e4ce5618fbbc7acc2db79999adc8d7292322227623a410147c8bca79449d31cee8e49e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sttrak::Application.config.session_store :cookie_store, key: '_sttrak_session'
